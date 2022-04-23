737ce4dcf65430ff89aa958f84dfdd0db423da26
ANA <ana@anniecannons.com>
2022-04-23 12:01:29 -0700

    commit to the 36000

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
 import Charts from './Components/Charts';
 import Graphs from './Components/Graphs';
+import * as d3 from "d3";
+import React, { useState, useRef, useEffect} from "react";
 
 
-function App() {
+const App=()=> {
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+
+  const [data, setData] = useState(generateData(0));
+  const changeData = () => {
+    setData(generateData());
+  };
+
+  useEffect(
+    () => {
+      setData(generateData());
+    },
+    [!data]
+  );
+
   return (
-    <div className="App">
-      <Charts />
-      <Graphs />
-    </div>
+    <div>
+        <span className="label">Multiple attackers</span>
+        <Charts 
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={0}
+          outerRadius={100}
+        />
+      </div>
+
   );
 }
 
