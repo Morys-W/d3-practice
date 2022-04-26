cadfd9b7a102004672be24a645e127bafd994950
ANA <ana@anniecannons.com>
2022-04-26 10:57:49 -0700

    practise graph

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import Charts from './Components/Charts';
-<<<<<<< HEAD
 import SimpleGraph from './Components/SimpleGraph';
 import TeeGraph from './Components/TeeGraph';
 
-=======
-import TeeGraph from './Components/TeeGraph';
->>>>>>> 98e627debbb7965539c9120647399929a2a544e0
 import * as d3 from "d3";
 import React, { useState, useRef, useEffect} from "react";
 
 
 const App=()=> {
-  const generateData = (value, length = 5) =>
-    d3.range(length).map((item, index) => ({
+  // const data = useState([
+  //   { date: 0, value: 1 },
+  //   { date: 1, value:  20},
+  //   { date: 2, value: 30},
+  //   { date: 3, value: 40 },
+  //   { date: 4, value: 50},
+
+  // ]);
+   const generateData = (value, length = 5) =>
+     d3.range(length).map((item, index) => ({
       date: index,
-      value: value === null || value === undefined ? Math.random() * 100 : value
+      value: 200,
     }));
 
-  const [data, setData] = useState(generateData(0));
-  const changeData = () => {
-    setData(generateData());
-  };
+  const [data, setData] = useState();
+  
 
   useEffect(
     () => {
       setData(generateData());
     },
-    [!data]
+    []
   );
 
   return (
--- src/Components/Charts.js
+++ src/Components/Charts.js
@@ -12,12 +12,16 @@ import * as d3 from "d3";
     .arc()
     .innerRadius(props.innerRadius)
     .outerRadius(props.outerRadius);
-  const colors = d3.scaleOrdinal(d3.schemeCategory10);
+  const colors = ['#5878A3','#E49343','#D0605E','#85B5B1','#6B9F57']
   const format = d3.format(".2f");
 
   useEffect(
     () => {
+      if (props.data){
+
+      
       const data = createPie(props.data);
+      console.log(data);
       const group = d3.select(ref.current);
       const groupWithData = group.selectAll("g.arc").data(data);
 
@@ -35,14 +39,15 @@ import * as d3 from "d3";
       path
         .attr("class", "arc")
         .attr("d", createArc)
-        .attr("fill", (d, i) => colors(i));
+        .attr("fill", (d, i) => colors[i]);
 
       const text = groupWithUpdate
         .append("text")
         .merge(groupWithData.select("text"));
 
-    
+      };
     },
+
     [props.data]
   );
 
