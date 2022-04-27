b0d1dab28d657bac687367b2516cf3a4badeca60
Morys <Morys@anniecannons.com>
2022-04-27 14:53:21 -0500

    no message

---
+++ src/Components/Data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        gender: 'female',
+        value: 10
+    },
+    {
+        gender: 'male',
+        value: 90
+    },
+    {
+        gender: 'transgender female',
+        value: 5
+    },
+    {
+        gender: 'transgender male',
+        value: 20
+    },
+    {
+        gender: "I don't know",
+        value: 30
+    }
+];
---
+++ src/Pages/Home.js
@@ -0,0 +1,62 @@
+import Charts from '../Components/Charts';
+
+import TeeGraph from '../Components/TeeGraph';
+import Data from '../Components/Data';
+import { Element } from 'react-faux-dom';
+
+import * as d3 from "d3";
+import React, { useState, useEffect} from "react";
+
+
+const Home = () => {
+  const generateData = (value, length = 5) =>
+    d3.range(length).map((item, index) => ({
+      date: index,
+      value: value === null || value === undefined ? Math.random() * 100 : value
+    }));
+
+  const [data, setData] = useState(generateData(0));
+  // const changeData = () => {
+  //   setData(generateData());
+  // };
+
+  useEffect(
+    () => {
+      setData(generateData());
+    },
+    [!data]
+  );
+
+  return (
+    <div>
+        <span className="label">Multiple attackers</span>
+        <Charts 
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={0}
+          outerRadius={100}
+        />
+        <TeeGraph 
+        />
+      </div>
+
+  );
+}
+
+export default Home;
+
+{/* <SimpleGraph 
+  width={600}
+  height={350}
+  data={{
+  columns: [
+    ['female', 30],
+    ['male', 150],
+    ['transgender female', 5],
+    ['transgender male', 40],
+    ["I don't know", 10]
+  ],
+  type: 'bar',
+  }}
+/> */}
